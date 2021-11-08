72e3d06950473827e1f144590f5f7d57daab8e93
edguitar1986 <93894334+edguitar1986@users.noreply.github.com>
2021-11-07 22:33:45 -0500

    Add files via upload

---
+++ Ejercicio1-parte 1-Edgar Lincango.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 20:12:02 2021
+
+@author: Edgar Lincango
+"""
+
+print("Consultando los tipos de datos")
+
+print("El tipo de datos de 875 es:")
+print(type(875))
+
+print("El tipo de datos de 4.89 es:")
+print(type(4.89))
+
+print("El tipo de datos del texto: Now is better than never, es:")
+print(type("Now is better than never"))
+
+print("El tipo de datos de 1.32 es:")
+print(type(1.32))
+
+print("El tipo de datos de 5+8i es:")
+print(type(5+8j))
+
+print("¿El valor 8.2 corresponde a unvalor entero?:")
+print(isinstance(8.2,int))
+
+print("¿El texto: Reality counts, corresponde a un string?:")
+print(isinstance("Reality counts", str))
+
